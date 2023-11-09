17d955abf43c76cb6f07fab3cc9633c4e7fe7ade
Arthur Bartoli <arthurbartoli5@gmail.com>
2023-11-09 17:32:02 +0100

    Revert "Features planing"
    This reverts commit 224bb3fad1ae178f517fd8b134da63f27e567609.

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ class Program {
     static void Main(string[] args) {
         var view = new ConsoleView();
         // var controller = new RssReaderController();
-        Feed feed_test = RssReaderController.LoadFeed("https://www.lemonde.fr/rss/une.xml");
-        RssReaderController.DisplayFeed(feed_test);
+        Feed feed_test = RssReaderController.LoadFeed("https://www.feedforall.com/sample.xml");
+        view.DisplayArticle(feed_test.Articles[1]);
     }
 }
