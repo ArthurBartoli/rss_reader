edc76255177b61d7f96345d768e7c81a5d3c685b
Arthur Bartoli <arthurbartoli5@gmail.com>
2023-11-14 15:47:19 +0100

    Add tests
    tests: tests for Link and LastBuildDate of feed and pubDate for article (name changed)

--- rss_reader_tests/UnitTest1.cs
+++ rss_reader_tests/UnitTest1.cs
@@ -20,6 +20,8 @@ public class FeedFetchingAndLoading
         Assert.Equal("FeedForAll Sample Feed", feed_test.Title);
         Assert.Equal("RSS is a fascinating technology. The uses for RSS are expanding daily. Take a closer look at how various industries are using the benefits of RSS in their businesses.", feed_test.Description);
         Assert.Equal("Computers/Software/Internet/Site Management/Content Management", feed_test.Category);
+        Assert.Equal("http://www.feedforall.com/industry-solutions.htm", feed_test.Link);
+        Assert.Equal("Tue, 19 Oct 2004 13:39:14 -0400", feed_test.LastBuildDate);
     }
 
     [Fact]
@@ -36,7 +38,7 @@ public class FeedFetchingAndLoading
         // Assert
         Assert.Equal("RSS Solutions for Restaurants", article_test.Title);
         Assert.Equal("<b>FeedForAll </b>helps Restaurant's communicate with customers. Let your customers know the latest specials or events.<br>\n<br>\nRSS feed uses include:<br>\n<i><font color=\"#FF0000\">Daily Specials <br>\nEntertainment <br>\nCalendar of Events </i></font>", article_test.Description);
-        Assert.Equal("Tue, 19 Oct 2004 11:09:11 -0400", article_test.Date);
+        Assert.Equal("Tue, 19 Oct 2004 11:09:11 -0400", article_test.pubDate);
         Assert.Equal("http://www.feedforall.com/restaurant.htm", article_test.Link);
     }
 }
