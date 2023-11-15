b2a6c9b8bf4fd0549cbe2a76eb2f57ae0d2d28d0
Arthur Bartoli <arthurbartoli5@gmail.com>
2023-11-15 11:54:02 +0100

    Update some debug code
    chores: refactored some debug code so it's nicer

--- Program.cs
+++ Program.cs
@@ -9,10 +9,14 @@ class Program {
         var feed_list = new FeedList();
         var view = new ConsoleView();
 
-        RssReaderController.AddFeed(feed_list, "https://www.feedforall.com/sample.xml");
-        RssReaderController.AddFeed(feed_list, "https://www.feedforall.com/sample-feed.xml");
-        RssReaderController.AddFeed(feed_list, "https://www.feedforall.com/blog-feed.xml");
-        RssReaderController.AddFeed(feed_list, "http://www.rss-specifications.com/blog-feed.xml");
+        string[] sample_feeds = new string[4] { 
+            "https://www.feedforall.com/sample.xml", 
+            "https://www.feedforall.com/sample-feed.xml", 
+            "https://www.feedforall.com/blog-feed.xml",
+            "http://www.rss-specifications.com/blog-feed.xml"
+        };
+
+        foreach (string feed in sample_feeds) { feed_list = RssReaderController.AddFeed(feed_list, feed); }
 
         view.ListFeed(feed_list);   
     }
