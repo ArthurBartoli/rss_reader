229b6a01108d5870442326fb267cf34aa1560b75
Arthur Bartoli <arthurbartoli5@gmail.com>
2023-11-22 15:11:12 +0100

    Add available export list
    feat: added the ListExports method to the RssReaderController in order to display quickly and efficiently the available exports

--- Program.cs
+++ Program.cs
@@ -8,7 +8,14 @@ class Program {
     static void Main(string[] args) {
         FeedList feed_list = new FeedList();
         FeedList feed_list2 = new FeedList();
+        string SOLUTION_DIR = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\..\rss_reader");
 
         ConsoleView.TitleScreen();
+        Console.WriteLine("Select one of the available feed list :");
+        foreach (var item in RssReaderController.ListExports())
+        {
+            Console.WriteLine("* " + Path.GetFileName(item));
+        }
+        // GET LIST OF EXPORTS
     }
 }
--- controllers/RssReaderController.cs
+++ controllers/RssReaderController.cs
@@ -8,10 +8,34 @@ using rss_reader.models;
 using Views;
 using Controllers;
 using rss_reader.controllers;
+using System.IO;
 
 namespace Controllers{
     class RssReaderController
     {
+
+        public static string[] ListExports()
+        {
+            try
+            {
+                string exportDirectory = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\..\rss_reader\export\");
+                string[] TXTFiles = Directory.GetFiles(exportDirectory, "*.txt");
+                if (TXTFiles.Length == 0)
+                {
+                    string[] tmp = new string[1] { "No exports found." };
+                    return tmp;
+                }
+
+                return TXTFiles;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error while listing existing exports");
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
 
         public static FeedList AddFeed(FeedList feedList, string url) 
         {   
