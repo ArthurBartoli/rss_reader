dcf7e22183b9e94ef83e6a4db01695f45578e678
Arthur Bartoli <arthurbartoli5@gmail.com>
2023-12-13 11:02:12 +0100

    Add avalonia project
    The avalonia project has been added to the solution and can now be built upon :)

---
+++ rss_reader_gui/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace rss_reader_gui.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ rss_reader_gui/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace rss_reader_gui.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
