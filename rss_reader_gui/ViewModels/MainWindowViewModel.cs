5dbcb59ba8b7e425d58e4a747c00ab08a1fb97e9
Arthur Bartoli <arthurbartoli5@gmail.com>
2024-01-18 16:37:56 +0100

    Add feed selection
    feat: The feed can be selected and articles display on the right and can be selected. Soon they shall be displayed !

--- rss_reader_gui/ViewModels/MainWindowViewModel.cs
+++ rss_reader_gui/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,7 @@
-﻿using rss_reader.models;
+﻿using Avalonia.Controls;
+using rss_reader.models;
 using rss_reader_gui.Models;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
@@ -17,16 +19,20 @@ namespace rss_reader_gui.ViewModels
             RepositoryCentral.SelectedExportChanged += LoadFeedList;
             List<string> FullExportsList = new() { "Nothing to say !" };
             Feeds = new ObservableCollection<string>(FullExportsList);
+            Articles = new ObservableCollection<string>(FullExportsList);
         }
         public FeedList feedList { get; set; }
 
         public async void LoadFeedList(object sender, SelectedExportChangedEventArgs e)
         {
+            // Import all feeds into the feedlist object
             feedList = new FeedList();
             string relativePath = @"..\..\..\..\..\rss_reader\export\";
             string absoluteExportDirPath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), relativePath));
             string EXPORT_PATH = Path.Combine(absoluteExportDirPath, e.SelectedExport.ToString() + ".txt");
             await feedList.ImportListAsync(EXPORT_PATH);
+
+            // Displaying the list of available feeds
             List<string> feedNames = new List<string>();
             foreach (string k in feedList.Feeds.Keys)
             {
@@ -35,6 +41,56 @@ namespace rss_reader_gui.ViewModels
             Feeds = new ObservableCollection<string>(feedNames);
         }
 
+        public void OnFeedSelect(object sender, SelectionChangedEventArgs e)
+        {
+            string selection = e.ToString();
+            var feedKey = feedList.Feeds.First(kvp => kvp.Value.Equals(selection)).Key;
+            Feed selected_feed = feedList.Feeds[feedKey];
+
+            List<string> articleNames = new();
+            foreach (Article item in selected_feed.Articles.Values)
+            {
+                articleNames.Add(item.Title);
+            }
+            Articles = new ObservableCollection<string>(articleNames);
+        }
+
+
+        private ObservableCollection<string> _article;
+
+        public ObservableCollection<string> Article
+        {
+            get { return _article; }
+            set { SetProperty(ref _article, value); }
+        }
+
+        private string _feed;
+
+        public string Feed
+        {
+            get { return _feed; }
+            set { 
+                SetProperty(ref _feed, value);
+                string selection = Feed;
+                var feedKey = feedList.Feeds.First(x => x.Value.Title == selection).Key;
+                Feed selected_feed = feedList.Feeds[feedKey];
+
+                List<string> articleNames = new();
+                foreach (Article item in selected_feed.Articles.Values)
+                {
+                    articleNames.Add(item.Title);
+                }
+                Articles = new ObservableCollection<string>(articleNames);
+            }
+        }
+
+        private ObservableCollection<string> _articles;
+
+        public ObservableCollection<string> Articles
+        {
+            get { return _articles; }
+            set { SetProperty(ref _articles, value); }
+        }
 
         private ObservableCollection<string> _feeds;
 
@@ -43,9 +99,7 @@ namespace rss_reader_gui.ViewModels
             get { return _feeds; }
             set { SetProperty(ref _feeds, value); }
         }
-        /// <summary>
-        /// ///////////////
-        /// </summary>
+
         private ObservableCollection<string> _tmp;
 
         public ObservableCollection<string> tmp
