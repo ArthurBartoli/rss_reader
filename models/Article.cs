767d89e8946643825fe1419ddb50b7fdbc7cb922
Arthur Bartoli <arthurbartoli5@gmail.com>
2024-02-15 16:31:54 +0100

    Untrack files in .gitignore

--- models/Article.cs
+++
@@ -1,9 +0,0 @@
-namespace Models {
-    class Article {
-        public string Title { get; set; }
-        public string Link { get; set; }
-        public string PubDate { get; set; }
-        public string Description { get; set; }
-
-    }
-}
