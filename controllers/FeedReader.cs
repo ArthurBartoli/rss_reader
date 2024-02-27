7e0b99de217e0b01fd2622436e041b31a376c72a
Arthur Bartoli <77788177+ArthurBartoli@users.noreply.github.com>
2024-02-27 15:32:31 +0100

    Add documentation
    doc: All documentation for the FeedReader object

--- controllers/FeedReader.cs
+++ controllers/FeedReader.cs
@@ -4,14 +4,29 @@ using System.Xml.Linq;
 
 namespace rss_reader.controllers
 {
+    /// <summary>
+    /// Centralizes getting the xml feed from the url, parsing the xml to get feed info 
+    /// and reading of the parsing output. 
+    /// </summary>
     class FeedReader
     {
+        /// <summary>
+        /// Fetches the XML Feed from the url adress.
+        /// </summary>
+        /// <param name="url">Url to the feed XML</param>
+        /// <remarks>This method needs to be called asynchronously or by awaiting results.</remarks>
+        /// <returns>A string task</returns>
         private static async Task<string> GetFeedXml(string url)
         {
             using var client = new HttpClient(); // to send query and read response
             return await client.GetStringAsync(url); // await the http answer
         }
-
+        /// <summary>
+        /// Asynchronous version of the <see cref="ParseFeed"/> method because it awaits for the URL response
+        /// of the <see cref="GetFeedXml(string)"/> method.
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
         private static async Task<XDocument?> ParseFeedAsync(string url)
         {
             try
@@ -26,7 +41,11 @@ namespace rss_reader.controllers
                 return null;
             }
         }
-
+        /// <summary>
+        /// Parses the XML object returned by the <see cref="GetFeedXml(string)"/> method.
+        /// </summary>
+        /// <param name="url">Url to the feed XML</param>
+        /// <returns>An XDocument object containing the parsed XML feed.</returns>
         private static XDocument? ParseFeed(string url)
         {
             try
@@ -41,7 +60,12 @@ namespace rss_reader.controllers
                 return null;
             }
         }
-
+        /// <summary>
+        /// Asynchronous version of <see cref="ReadFeed"/> because it awaits for the URL response
+        /// of the <see cref="GetFeedXml(string)"/> method.
+        /// </summary>
+        /// <param name="url">Url to the feed XML</param>
+        /// <returns>A completed Feed object with a dict of completed Articles.</returns>
         public static async Task<Feed>? ReadFeedAsync(string url)
         {
             try
@@ -77,6 +101,15 @@ namespace rss_reader.controllers
                 return null;
             }
         }
+        /// <summary>
+        /// Reads the XDocument object returned by the <see cref="ParseFeed(string)"/> method to get the 
+        /// title, description, category and date of last build of this feed. The same is done for every
+        /// article in the Feed corresponding to the url by reading the title, link, date of publication
+        /// and description.
+        /// All this information is stored into a Feed object containing a dict of Articles objects.
+        /// </summary>
+        /// <param name="url">Url to the feed XML</param>
+        /// <returns>A completed Feed object with a dict of completed Articles.</returns>
         public static Feed? ReadFeed(string url)
         {
             try
