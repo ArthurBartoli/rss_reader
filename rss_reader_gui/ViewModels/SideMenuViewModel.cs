2c51dfa1b270973251288d4408ff28301d103f61
Arthur Bartoli <arthurbartoli5@gmail.com>
2024-02-28 18:02:10 +0100

    Add doc
    doc: For the side menu View and ViewModel

--- rss_reader_gui/ViewModels/SideMenuViewModel.cs
+++ rss_reader_gui/ViewModels/SideMenuViewModel.cs
@@ -5,20 +5,31 @@ using System.Linq;
 
 namespace rss_reader_gui.ViewModels
 {
+    /// <summary>
+    /// Avalonia view-model class for handling the logic and data behind the side menu.
+    /// This view will display a list of exports handled by the RSSDataManagement object.
+    /// </summary>
     public partial class SideMenuViewModel : ViewModelBase
     {
+        // TODO: REMOVE THIS
 #pragma warning disable CA1822 // Mark members as static
         public string Greeting => "This is the side menu!";
 #pragma warning restore CA1822 // Mark members as static
 
         private ObservableCollection<string> _exports;
-
+        /// <summary>
+        /// Gets and sets exports as an <see cref="ObservableCollection{T}">observable collection</see> for listing.
+        /// </summary>
         public ObservableCollection<string> Exports
         {
             get { return _exports; }
             set { SetProperty(ref _exports, value); }
         }
 
+        /// <summary>
+        /// Initializes a new instance of the SideMenuViewModel class by listing exports and exposing this
+        /// list as an <see cref="ObservableCollection{T}">Observable Collection</see>.
+        /// </summary>
         public SideMenuViewModel()
         {
             List<string> FullExportsList = RSSDataManagement.ListExports().Values.Select(_ => _.Item1).ToList();
