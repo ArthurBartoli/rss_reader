33b48967c625d77584b4e42eb0a9da1e4a33819d
Arthur Bartoli <77788177+ArthurBartoli@users.noreply.github.com>
2024-02-28 11:24:52 +0100

    Add documentation
    doc: documentation for the toolbox, but more specifically for the FindKeyFromItem method

--- toolbox/FindKeyFromItem.cs
+++ toolbox/FindKeyFromItem.cs
@@ -4,6 +4,14 @@ namespace rss_reader.toolbox
 {
     static public class DictTools
     {
+        /// <summary>
+        /// Returns the key of a dictionary of Articles corresponding to an article title. 
+        /// If multiple keys correspond, then the first encountered will be returned, which is ok
+        /// since it would be surprising to have two articles with the same title.
+        /// </summary>
+        /// <param name="dict">Dict in which to search the key.</param>
+        /// <param name="val">Title of the article to search.</param>
+        /// <returns>The key as a string.</returns>
         static public string GetKeyFromItem(Dictionary<string, Article> dict, string val)
         {
             foreach (KeyValuePair<string, Article> paire in dict)
